b243fd2e812bdf01f83bdde26c7c39b1c47d9c6e
MLMIP <67742308+MLMIP@users.noreply.github.com>
2025-10-26 19:42:20 +0800

    Delete CLOVER directory

--- CLOVER/script/cifar100/40+20x3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=0 python src/main_incremental.py --approach clover --gmms 1 --max-experts 5 --use-multivariate --nepochs 200 --tau 3 --batch-size 128 --num-workers 8 --datasets cifar100_icarl_224 --num-tasks 21 --nc-first-task 40 --lr 0.1 --weight-decay 1e-4 --ft-lr 0.1 --ft-weight-decay 1e-4 --clipping 1 --alpha 0.99 --ftepochs 0 --use-test-as-val --network resnet18 --extra-aug fetril --momentum 0.9 --exp-name cifar40+20x3 --seed 0 --shared 1 --w0 0.10 --w1 0.05 --save-models
--- CLOVER/script/cifar100/50+5x10.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=0 python src/main_incremental.py --approach clover --gmms 1 --max-experts 5 --use-multivariate --nepochs 200 --tau 3 --batch-size 128 --num-workers 8 --datasets cifar100_icarl_224 --num-tasks 6 --nc-first-task 50 --lr 0.1 --weight-decay 1e-4 --ft-lr 0.1 --ft-weight-decay 1e-4 --clipping 1 --alpha 0.99 --ftepochs 0 --use-test-as-val --network resnet18 --extra-aug fetril --momentum 0.9 --exp-name cifar50+5x10 --seed 0 --shared 1 --w0 0.10 --w1 0.05 --save-models
